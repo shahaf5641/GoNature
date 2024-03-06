2d81bcaad3f2376ccad475000b08341fa7e1afe6
sarasal1 <prsarasal@gmail.com>
2024-03-06 15:33:44 +0200

    htfdtt

--- G21_Assignment3/G21_Common/src/alerts/CustomAlerts.java
+++ G21_Assignment3/G21_Common/src/alerts/CustomAlerts.java
@@ -6,7 +6,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
- * CustomAlerts is a customize Alerts blabl
+ * CustomAlerts is a customize Alerts blablyhtt
  *
  */
 public class CustomAlerts extends Alert {
