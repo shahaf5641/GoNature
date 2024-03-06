b659fa6efeba1a2a402e1804d1c80c8c67e70024
saras <saras@LAPTOP-B5QH5883>
2024-03-06 15:02:37 +0200

    blaaaaaa

--- G21_Assignment3/G21_Common/src/alerts/CustomAlerts.java
+++ G21_Assignment3/G21_Common/src/alerts/CustomAlerts.java
@@ -6,7 +6,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
- * CustomAlerts is a customize Alerts blabla
+ * CustomAlerts is a customize Alerts blablblalalla
  *
  */
 public class CustomAlerts extends Alert {
