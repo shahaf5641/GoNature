59e6b98191b69a89557574bc9f14c680e54c30ea
sarasal1 <prsarasal@gmail.com>
2024-03-11 18:12:04 +0200

    deleted

--- G21_Assignment3/G21_Common/src/alerts/CustomAlerts.java
+++ G21_Assignment3/G21_Common/src/alerts/CustomAlerts.java
@@ -6,7 +6,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
- * CustomAlerts is a customize Alerts blalalallalall
+ * CustomAlerts is a customize Alerts blalalallall
  *
  */
 public class CustomAlerts extends Alert {
