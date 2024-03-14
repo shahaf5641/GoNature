a999011214791eeb9dca4f36ddb5d06d39a1b175
yuavlsabato@gmail.com <yuval@LAPTOP-35TH513N>
2024-03-14 12:53:14 +0200

    change

--- G21_Assignment3/G21_GoNatureServer/src/controllers/EmailControl.java
+++ G21_Assignment3/G21_GoNatureServer/src/controllers/EmailControl.java
@@ -15,125 +15,127 @@ import logic.Messages;
 import server.GoNatureServer;
 
 /**
- * EmailControl class handle all the Email sends requests
- * 
+ * EmailControl class handles all the email sending requests.
  */
 public class EmailControl {
-	
-	private static TravelersQueries travelersQueries = new TravelersQueries(GoNatureServer.mysqlconnection);
+    
+    // Access the TravelersQueries object for retrieving email addresses
+    private static TravelersQueries travelersQueries = new TravelersQueries(GoNatureServer.mysqlconnection);
+    
+    // Sender email and password
     private static String senderEmail = GoNatureFinals.GO_NATURE_EMAIL;
     private static String senderPassword = GoNatureFinals.GO_NATURE_EMAIL_PASSWORD;
     
-    
     /**
-     * This function gets a Message object and send the message to the traveler.
+     * Sends an email to the traveler based on the provided message.
      * 
-     * @param msg the message to send
-     * @return true on success, false otherwise
+     * @param msg The message to send.
+     * @return True if the email is sent successfully, false otherwise.
      */
     public static boolean sendEmail(Messages msg) {
-    	String sendTo = travelersQueries.getEmailByOrderID(msg.getOrderId());
-    	String subject = msg.getSubject();
-		String messageToSend = msg.getContent();
-		return sendEmail(sendTo, subject, messageToSend);
-    	
+        String sendTo = travelersQueries.getEmailByOrderID(msg.getOrderId());
+        String subject = msg.getSubject();
+        String messageToSend = msg.getContent();
+        return sendEmail(sendTo, subject, messageToSend);
     }
     
     /**
-     * This function gets a Message object and send the message to GoNature email.
+     * Sends an email to GoNature email based on the provided message.
      * 
-     * @param msg the message to send
-     * @return true on success, false otherwise
+     * @param msg The message to send.
+     * @return True if the email is sent successfully, false otherwise.
      */
     public static boolean sendEmailToGoNature(Messages msg) {
-    	String sendTo = GoNatureFinals.GO_NATURE_EMAIL;
-    	String subject = msg.getSubject();
-		String messageToSend = msg.getContent();
-		return sendEmail(sendTo, subject, messageToSend);
-    	
+        String sendTo = GoNatureFinals.GO_NATURE_EMAIL;
+        String subject = msg.getSubject();
+        String messageToSend = msg.getContent();
+        return sendEmail(sendTo, subject, messageToSend);
     }
     
     /**
-     * This function gets a Message object and send the message to the given email.
+     * Sends an email to the provided email address based on the provided message.
      * 
-     * @param msg the message to send
-     * @param email the email to send to
-     * @return true on success, false otherwise
+     * @param msg The message to send.
+     * @param email The email address to send to.
+     * @return True if the email is sent successfully, false otherwise.
      */
     public static boolean sendEmailToWithEmailInput(Messages msg, String email) {
-    	String sendTo = email;
-    	String subject = msg.getSubject();
-		String messageToSend = msg.getContent();
-		return sendEmail(sendTo, subject, messageToSend);
-    	
+        String sendTo = email;
+        String subject = msg.getSubject();
+        String messageToSend = msg.getContent();
+        return sendEmail(sendTo, subject, messageToSend);
     }
     
-	/* Send an email
-	 * Returns true on success
-	 */
-	private static boolean sendEmail(String sendTo, String subject, String messageToSend) {
-		
+    /**
+     * Sends an email.
+     * 
+     * @param sendTo The recipient email address.
+     * @param subject The email subject.
+     * @param messageToSend The content of the email.
+     * @return True if the email is sent successfully, false otherwise.
+     */
+    private static boolean sendEmail(String sendTo, String subject, String messageToSend) {
         Properties props = getProperties();
-
         Session session = Session.getInstance(props, new Authenticator() {
-        	protected PasswordAuthentication getPasswordAuthentication() {
-        		return new PasswordAuthentication(senderEmail, senderPassword);
-        	}
-		});
-        
-        
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, senderPassword);
+            }
+        });
         Message msg = prepareMessage(session, senderEmail, sendTo, subject, messageToSend);
         if (msg != null) {
-        	try {
-                // Send message  
-                Transport.send(msg);  
+            try {
+                // Send message
+                Transport.send(msg);
                 return true;
-			} catch (Exception e) {
-				System.out.println("Faild to send email");
-		        return false;
-			}
+            } catch (Exception e) {
+                System.out.println("Failed to send email");
+                return false;
+            }
         }
-
         return false;
-	}
-	
-	/* Create the Message object
-	 * The message object includes all the details needed to send an email:
-	 * Sender email, Recipient Email, Email's subject and the email's body
-	 */
-	private static Message prepareMessage(Session session, String from, String to, String subject, String messageToSend) {
-		Message message = new MimeMessage(session); 
+    }
+    
+    /**
+     * Prepares the email message.
+     * 
+     * @param session The email session.
+     * @param from The sender email address.
+     * @param to The recipient email address.
+     * @param subject The email subject.
+     * @param messageToSend The content of the email.
+     * @return The prepared email message.
+     */
+    private static Message prepareMessage(Session session, String from, String to, String subject, String messageToSend) {
+        Message message = new MimeMessage(session);
         try {
-			message.setFrom(new InternetAddress(from));
-	        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));  
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject(subject);
             message.setText(messageToSend);
             message.setContent(messageToSend, "text/plain; charset=\"UTF-8\"");
             return message;
-		} catch (MessagingException e) {
-			System.out.println("Something went wrong while creating the message object");
-			e.printStackTrace();
-		}
+        } catch (MessagingException e) {
+            System.out.println("Something went wrong while creating the message object");
+            e.printStackTrace();
+        }
         return null;
-
-	}
-	
-	/* Create the Properties object
-	 * The Properties object includes all the server properties.
-	 * We are using the Gmail server to send an email
-	 */
-	private static Properties getProperties() {
+    }
+    
+    /**
+     * Retrieves the email server properties.
+     * 
+     * @return The email server properties.
+     */
+    private static Properties getProperties() {
         Properties props = new Properties();
-        props.setProperty("mail.transport.protocol", "smtp");     
-        props.setProperty("mail.host", "smtp.gmail.com");  
-        props.put("mail.smtp.auth", "true");  
-        props.put("mail.smtp.port", "465");  
-        props.put("mail.debug", "false");  
-        props.put("mail.smtp.socketFactory.port", "465");  
-        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");  
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", "smtp.gmail.com");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.debug", "false");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
-        
-		return props; 
-	}
-
+        return props;
+    }
 }
